88b588b96c226ebb98f5b9743af4214e46e524fa
Dena Neuman <denaweiss5@gmail.com>
2020-10-22 19:26:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,10 +24,10 @@ def end_game(card_total)
 end
 
 def initial_round
+  display_card_total(card_total)
   deal_card
   deal_card
   return deal_card + deal_card
-  puts display_card_total(card_total)
 end
 
 def hit? (number)
